7e0acfdac391c631366b787599f92bcc908ec71a
Nicolas Iselin <nicolas.github@iselin.ch>
2026-07-30 23:59:44 +0200

    test: scene defs

---
+++ tests/model/scene/test_defs.py
@@ -0,0 +1,23 @@
+
+import pytest
+from dataclasses import FrozenInstanceError
+from svgtools.model.scene.defs import Defs
+
+def test_defs_construction():
+    d = Defs(("x", 2))
+    assert d.children == ( "x", 2 )
+    assert d.children[0] == "x"
+    assert d.children[1] == 2
+
+def test_defs_are_equal():
+    assert Defs(("y", 1)) == Defs(("y", 1))
+
+def test_defs_is_immutable():
+    d = Defs(("x", 2))
+    with pytest.raises(FrozenInstanceError):
+        d.children = ("x", 3)
+
+def test_empty_defs():
+    d = Defs(children=())
+    assert d.children == ()
+
