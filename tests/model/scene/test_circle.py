6885526a5e7121ebb40377f820bbe6ed05c8373a
Nicolas Iselin <nicolas.github@iselin.ch>
2026-07-31 01:38:59 +0200

    test: Scene Circle

---
+++ tests/model/scene/test_circle.py
@@ -0,0 +1,22 @@
+
+import pytest
+from dataclasses import FrozenInstanceError
+from svgtools.model.geometry.point import Point as GeometryPoint
+from svgtools.model.geometry.circle import Circle as GeometryCircle
+from svgtools.model.scene.circle import Circle
+
+def test_circle_construction():
+    c = Circle(geometry=(GeometryCircle(GeometryPoint(1.5, -2.0), 2)))
+    assert c.geometry.center.x == 1.5
+    assert c.geometry.center.y == -2.0
+    assert c.geometry.radius == 2
+
+def test_circles_are_equal():
+    assert ( Circle(geometry=(GeometryCircle(GeometryPoint(1.5, -2.0), 2))) ==
+             Circle(geometry=(GeometryCircle(GeometryPoint(1.5, -2.0), 2)))
+           )
+
+def test_circle_is_immutable():
+    c = Circle(geometry=(GeometryCircle(GeometryPoint(1.5, -2.0), 2)))
+    with pytest.raises(FrozenInstanceError):
+       c.geometry.radius = 3
