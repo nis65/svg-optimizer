8eb482b7dde8289712d787ca5bb8bb605a230f3e
Nicolas Iselin <nicolas.github@iselin.ch>
2026-07-31 16:47:09 +0200

    feat: parser parses empty svg

--- src/svgtools/parser.py
+++ src/svgtools/parser.py
@@ -1,9 +1,22 @@
+from xml.etree import ElementTree as ET
+
 from svgtools.model.scene.document import Document
 from svgtools.model.scene.svg import Svg
 
-def parse_string(svg: str):
+def parse_string(svg_text: str) -> Document:
+
+    xml_root = ET.fromstring(svg_text)
+
+    if xml_root.tag != "svg":
+        raise ValueError("Root element must be <svg>")
+
+    scene_children = []
+
+    for xml_child in xml_root:
+        raise NotImplementedError
+
     return Document(
         svg=Svg(
-            children=(),
+            children=( tuple(scene_children) ),
         )
     )
