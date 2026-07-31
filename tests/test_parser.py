ce87da95979221459a065f59ec7b7cfa931290ca
Nicolas Iselin <nicolas.github@iselin.ch>
2026-07-31 17:12:39 +0200

    test: Test svg with empty defs

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,9 @@
-from svgtools.parser import parse_string
+import pytest
 
+from svgtools.parser import parse_string
 from svgtools.model.scene.document import Document
 from svgtools.model.scene.svg import Svg
+from svgtools.model.scene.defs import Defs
 
 
 def test_parse_empty_svg():
@@ -11,4 +13,25 @@ def test_parse_empty_svg():
         svg=Svg(
             children=(),
         )
+    )
+
+def test_root_element_must_be_svg():
+    with pytest.raises(ValueError, match="Root element must be <svg>, not"):
+        parse_string("<circle/>")
+
+def test_parse_empty_defs():
+    svg_text = """
+    <svg>
+        <defs/>
+    </svg>
+    """
+
+    assert parse_string(svg_text) == Document(
+        svg=Svg(
+            children=(
+                Defs(
+                    children=(),
+                ),
+            ),
+        ),
     )
