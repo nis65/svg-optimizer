7970cea6b8e8439d9d2a23fd135ae0547944ab87
Nicolas Iselin <nicolas.github@iselin.ch>
2026-07-31 18:26:57 +0200

    feat: parser with mutual recursion

--- src/svgtools/parser.py
+++ src/svgtools/parser.py
@@ -11,19 +11,23 @@ def parse_string(svg_text: str) -> Document:
     if xml_root.tag != "svg":
         raise ValueError(f"Root element must be <svg>, not '{xml_root.tag}'")
 
-    scene_children = []
-
-    for xml_child in xml_root:
-        scene_children.append(_parse_xml_element(xml_child))
-
     return Document(
         svg=Svg(
-            children=( tuple(scene_children) ),
+            children=_parse_xml_children(xml_root),
         )
     )
 
 def _parse_xml_element(xml_element: ET.Element):
 
     if xml_element.tag == "defs":
-         return Defs(children=())
+         return Defs(children=_parse_xml_children(xml_element))
     raise NotImplementedError("can parse only defs yet")
+
+def _parse_xml_children(xml_element: ET.Element) -> tuple:
+
+    scene_children = []
+
+    for xml_child in xml_element:
+        scene_children.append(_parse_xml_element(xml_child))
+
+    return tuple(scene_children)
