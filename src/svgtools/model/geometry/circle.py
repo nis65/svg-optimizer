499612f44bcb589ab56912e569a7b6b4efb1c719
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-01 21:08:45 +0200

    feat: bounding box for circle/rect

--- src/svgtools/model/geometry/circle.py
+++ src/svgtools/model/geometry/circle.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from .point import Point
+from .bounding_box import BoundingBox
 
 @dataclass(frozen=True, slots=True)
 class Circle:
@@ -11,3 +12,11 @@ class Circle:
             raise ValueError(
                 f"radius ({self.radius}) must not be negative"
             )
+
+    def bounding_box(self):
+        return BoundingBox(
+            min = Point(self.center.x - self.radius,
+                        self.center.y - self.radius),
+            max = Point(self.center.x + self.radius,
+                        self.center.y + self.radius)
+        )
