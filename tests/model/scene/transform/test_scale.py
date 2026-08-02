cbcffece7277825f8936c43f608d63698c119167
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-02 08:51:14 +0200

    test: transformations scale and translate

---
+++ tests/model/scene/transform/test_scale.py
@@ -0,0 +1,17 @@
+import pytest
+
+from dataclasses import FrozenInstanceError
+from svgtools.model.scene.transform.scale import Scale
+
+def test_scale_construction():
+    s = Scale(sx=3, sy=4)
+    assert s.sx == 3
+    assert s.sy == 4
+
+def test_scales_are_equal():
+    assert Scale(sx=3, sy=4) == Scale(sx=3, sy=4) 
+
+def test_scale_is_immutable():
+    s = Scale(sx=3, sy=4)
+    with pytest.raises(FrozenInstanceError):
+        s.sy = 5
---
+++ tests/model/scene/transform/test_translate.py
@@ -0,0 +1,17 @@
+import pytest
+
+from dataclasses import FrozenInstanceError
+from svgtools.model.scene.transform.translate import Translate
+
+def test_translate_construction():
+    t = Translate(dx=3, dy=4)
+    assert t.dx == 3
+    assert t.dy == 4
+
+def test_translates_are_equal():
+    assert Translate(dx=3, dy=4) == Translate(dx=3, dy=4)
+
+def test_translate_is_immutable():
+    t = Translate(dx=3, dy=4)
+    with pytest.raises(FrozenInstanceError):
+        t.dx = 4
