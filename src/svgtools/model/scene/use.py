1b2e9eb61b5c06f787226b809974540d30fef210
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-02 11:51:48 +0200

    feat: add transforms to scene elements

--- src/svgtools/model/scene/use.py
+++ src/svgtools/model/scene/use.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
+from .transform import Translate, Scale
 
 @dataclass(frozen=True, slots=True)
 class Use:
     href: str
     id: str | None = None
+    transformations: tuple[Translate | Scale, ...] = ()
