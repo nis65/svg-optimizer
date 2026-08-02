62d8fa013340afdd4826d0f13a79ef6f963fdb7d
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-02 11:51:27 +0200

    test: add transforms to scene elements

--- tests/model/scene/test_group.py
+++ tests/model/scene/test_group.py
@@ -2,13 +2,19 @@
 import pytest
 from dataclasses import FrozenInstanceError
 from svgtools.model.scene.group import Group
+from svgtools.model.scene.transform import Translate, Scale
 
 def test_group_construction():
     g = Group(id="gid", children=("x", 2))
     assert g.id == "gid"
+    assert g.transformations == ()
     assert g.children == ( "x", 2 )
     assert g.children[0] == "x"
     assert g.children[1] == 2
+
+def test_group_transformations():
+    g = Group(children=(), transformations = ( Scale ( 2, 3)) )
+    assert g.transformations == ( Scale ( 2, 3) )
 
 def test_groups_are_equal():
     assert Group(id="gid", children=("y", 1)) == Group(id="gid", children=("y", 1))
@@ -21,4 +27,3 @@ def test_group_is_immutable():
 def test_empty_group():
     g = Group(children=())
     assert g.children == ()
-
--- tests/model/scene/test_svg.py
+++ tests/model/scene/test_svg.py
@@ -1,14 +1,21 @@
 
 import pytest
 from dataclasses import FrozenInstanceError
+
 from svgtools.model.scene.svg import Svg
+from svgtools.model.scene.transform import Translate, Scale
 
 def test_svg_construction():
     svg = Svg(id="svgid", children=("x", 2))
     assert svg.id == "svgid"
+    assert svg.transformations == ()
     assert svg.children == ( "x", 2 )
     assert svg.children[0] == "x"
     assert svg.children[1] == 2
+
+def test_svg_transform():
+    svg = Svg(children=(), transformations = ( Translate ( 2, 3)) )
+    assert svg.transformations == ( Translate ( 2, 3) )
 
 def test_svgs_are_equal():
     assert Svg(id="svgid", children=("y", 1)) == Svg(id="svgid", children=("y", 1))
@@ -21,4 +28,3 @@ def test_svg_is_immutable():
 def test_empty_svg():
     svg = Svg(children=())
     assert svg.children == ()
-
