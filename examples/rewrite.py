2884ae7eab39d1f1839ef2be8e0a9ecb7bacd51c
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-06 01:15:25 +0200

    feat: add example programs

---
+++ examples/rewrite.py
@@ -0,0 +1,32 @@
+import argparse
+import sys
+
+#from svgtools.io import write_svg_file, parse_svg_file
+
+from svgtools.parser.svg_parser import parse_svg_string
+from svgtools.model.scene.document import Document
+from svgtools.writer.svg_writer import SvgWriter
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--in", dest="input")
+parser.add_argument("--out", dest="output")
+
+args = parser.parse_args()
+
+infile = open(args.input, encoding="utf-8") if args.input else sys.stdin
+outfile = open(args.output, "w", encoding="utf-8") if args.output else sys.stdout
+
+try:
+    svg_input_text = infile.read()
+    svg_doc = parse_svg_string(svg_input_text)
+    writer = SvgWriter()
+    outfile.write(writer.write_svg_string(svg_doc))
+
+finally:
+    if infile is not sys.stdin:
+        infile.close()
+    if outfile is not sys.stdout:
+        outfile.close()
+
+
