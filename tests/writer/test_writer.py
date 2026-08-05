a154cfe3a95c76153cfed5ca118b5cc27b42b4f7
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-05 16:24:49 +0200

    test: write rect

--- tests/writer/test_writer.py
+++ tests/writer/test_writer.py
@@ -8,12 +8,12 @@ from svgtools.model.scene.svg import Svg
 from svgtools.model.scene.defs import Defs
 from svgtools.model.scene.group import Group
 #from svgtools.model.scene.use import Use
-#from svgtools.model.scene.rect import Rect
+from svgtools.model.scene.rect import Rect
 #from svgtools.model.scene.circle import Circle
 from svgtools.model.scene.transform import Translate, Scale
-#from svgtools.model.geometry.rect import Rect as GeometryRect
+from svgtools.model.geometry.rect import Rect as GeometryRect
 #from svgtools.model.geometry.circle import Circle as GeometryCircle
-#from svgtools.model.geometry.point import Point as GeometryPoint
+from svgtools.model.geometry.point import Point as GeometryPoint
 
 def test_write_empty_svg():
     d = Document(
@@ -85,5 +85,35 @@ def test_write_empty_group_with_attributes():
     <?xml version='1.0' encoding='UTF-8'?>
     <svg>
     <g id="grpid" transform="scale(4 5)" />
+    </svg>
+    """)
+
+def test_write_rect_with_attributes():
+    d = Document(
+            svg=Svg(
+                children=(
+                    Rect(
+                        id="rectid",
+                        transformations=(
+                             Scale(sx=4, sy=5),
+                             Translate(dx=1, dy=2),
+                        ),
+                        geometry=GeometryRect(
+                            top_left=GeometryPoint(
+                                x=4,
+                                y=5,
+                            ),
+                            width=2,
+                            height=1,
+                        ),
+                    ),
+                )
+            )
+        )
+    writer = SvgWriter()
+    assert writer.write_svg_string(d) == dedent("""\
+    <?xml version='1.0' encoding='UTF-8'?>
+    <svg>
+    <rect id="rectid" x="4" y="5" width="2" height="1" transform="scale(4 5) translate(1 2)" />
     </svg>
     """)
