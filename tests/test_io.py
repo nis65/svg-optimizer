7ed9356d6a14944a76e1a43492906288897b992e
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-05 18:12:49 +0200

    test: read and write svg files

---
+++ tests/test_io.py
@@ -0,0 +1,17 @@
+
+from svgtools.io import write_svg_file, parse_svg_file
+
+from svgtools.model.scene.document import Document
+from svgtools.model.scene.svg import Svg
+
+def test_svg_doc_roundtrip_via_file(tmp_path):
+    filename = tmp_path / "test.svg"
+    d_before = Document(
+            svg=Svg(
+                children=(),
+            )
+        )
+    write_svg_file(d_before, filename)
+    assert filename.exists()
+    d_after = parse_svg_file(filename)
+    assert d_before == d_after
