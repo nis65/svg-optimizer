420e1bda9f97a601288b53d1c9e9fcf705504eff
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-08 00:40:25 +0200

    test: points_for_bounding_box and .isclose()

--- tests/model/geometry/test_point.py
+++ tests/model/geometry/test_point.py
@@ -16,3 +16,17 @@ def test_points_are_immutable():
 
     with pytest.raises(FrozenInstanceError):
         p.x = 3
+
+def test_points_are_close():
+    # test abs_tol near 0
+    p = Point(0,0)
+    q = Point(0.000000001,
+             -0.000000001)
+    assert not p.isclose(q,1e-10)
+    assert p.isclose(q,1e-9)
+    # test rel_tol off 0
+    p = Point(1000, 1)
+    q = Point(1000.00001, 1)
+    r = Point(1000.000001, 1)
+    assert not p.isclose(q,1e-20)
+    assert p.isclose(r,1e-20)
