702c31312f6f82613d0737b7450e4d418f91ef3c
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-08 12:28:57 +0200

    refactor: make points_are_equal part of Point and not test

--- src/svgtools/model/geometry/point.py
+++ src/svgtools/model/geometry/point.py
@@ -14,3 +14,18 @@ class Point:
         return (math.isclose(self.x, other.x, rel_tol=REL_TOL, abs_tol=scene_tol) and
                 math.isclose(self.y, other.y, rel_tol=REL_TOL, abs_tol=scene_tol)
         )
+
+    @staticmethod
+    def points_are_close(set1: set["Point"], set2: set["Point"], scene_tol: float) -> bool:
+        if len(set1) != len(set2):
+            return False
+        remaining = list(set2)
+        for point1 in set1:
+            for i, point2 in enumerate(remaining):
+                if point1.isclose(point2, scene_tol):
+                    remaining.pop(i)
+                    break
+            else:   
+                return False
+        return True
+
--- tests/model/geometry/test_circle.py
+++ tests/model/geometry/test_circle.py
@@ -1,4 +1,5 @@
 import pytest
+import math
 from dataclasses import FrozenInstanceError
 from svgtools.model.geometry.point import Point
 from svgtools.model.geometry.circle import Circle
@@ -33,36 +34,25 @@ def test_circle_bounding_box():
         max=Point(3, 3)
     )
 
-def points_are_close(set1: set[Point], set2: set[Point]) -> bool:
-    if len(set1) != len(set2):
-        return False
-    remaining = list(set2)
-    for point1 in set1:
-        for i, point2 in enumerate(remaining):
-            if point1.isclose(point2, 1e-8):
-                remaining.pop(i)
-                break
-            return False
-    return True
-
 def test_points_are_close():
     ps={Point(0,0),Point(1,1)}
-    assert points_are_close(ps, ps)
+    assert Point.points_are_close(ps, ps, 1e-8)
     qs={Point(1,1),Point(0,0)}
-    assert points_are_close(ps, qs)
+    assert Point.points_are_close(ps, qs, 1e-8)
     rs={Point(1.0000001,1),Point(0,0)}
-    assert not points_are_close(ps, rs)
+    assert not Point.points_are_close(ps, rs, 1e-8)
     ss={Point(1.00000001,1),Point(0,0)}
-    assert points_are_close(ps, ss)
+    assert Point.points_are_close(ps, ss, 1e-8)
 
 def test_circle_transformed_bounding_box():
     c = Circle(center=Point(0,0), radius=2)
-    assert c.points_for_bounding_box(4) == {
+    expected = {
         Point(0,2),
         Point(2,0),
         Point(0,-2),
         Point(-2,0),
     }
+    assert Point.points_are_close(expected, c.points_for_bounding_box(4), 1e-8)
     sqrt2 = math.sqrt(2)
     expected = {
         Point(0,2),
@@ -74,4 +64,4 @@ def test_circle_transformed_bounding_box():
         Point(-sqrt2, sqrt2),
         Point(-sqrt2, -sqrt2),
     }
-    assert points_are_close(expected, c.points_for_bounding_box(8))
+    assert Point.points_are_close(expected, c.points_for_bounding_box(8), 1e-8)
