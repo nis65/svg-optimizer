2fd3b5136372caebef40deae134c643dea1093ab
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-10 17:39:29 +0200

    test: support rotate

---
+++ tests/model/scene/transform/test_rotate.py
@@ -0,0 +1,18 @@
+import pytest
+
+from dataclasses import FrozenInstanceError
+from svgtools.model.scene.transform.rotate import Rotate
+
+def test_rotate_construction():
+    r = Rotate(theta=60, cx=3, cy=4)
+    assert r.theta == 60
+    assert r.cx == 3
+    assert r.cy == 4
+
+def test_rotates_are_equal():
+    assert Rotate(theta=60, cx=3, cy=4) == Rotate(theta=60, cx=3, cy=4)
+
+def test_rotate_is_immutable():
+    r = Rotate(theta=60, cx=3, cy=4)
+    with pytest.raises(FrozenInstanceError):
+        r.theta = 30
