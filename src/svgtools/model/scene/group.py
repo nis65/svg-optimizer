e4fb6e6746c8967c74b39423bd9a97188c25370b
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-10 18:49:23 +0200

    feat: more rotate tests

--- src/svgtools/model/scene/group.py
+++ src/svgtools/model/scene/group.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass, field
-from .transform import Translate, Scale
+from .transform import Translate, Scale, Rotate
 
 @dataclass(frozen=True, slots=True)
 class Group:
     children: tuple
     id: str | None = None
-    transformations: tuple[Translate | Scale, ...] = ()
+    transformations: tuple[Translate | Scale | Rotate, ...] = ()
     unknown_attributes: dict[str, str] = field(default_factory=dict)
--- src/svgtools/model/scene/rect.py
+++ src/svgtools/model/scene/rect.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass, field
 from svgtools.model.geometry.rect import Rect as GeometryRect
-from .transform import Translate, Scale
+from .transform import Translate, Scale, Rotate
 
 @dataclass(frozen=True, slots=True)
 class Rect:
     geometry: GeometryRect
     id: str | None = None
-    transformations: tuple[Translate | Scale, ...] = ()
+    transformations: tuple[Translate | Scale | Rotate, ...] = ()
     unknown_attributes: dict[str, str] = field(default_factory=dict)
