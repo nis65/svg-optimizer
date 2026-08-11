880eedc882220eab3848ea6173602d40a87c5137
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-11 07:16:58 +0200

    test: add scene skew and affine

---
+++ tests/model/scene/transform/test_affine.py
@@ -0,0 +1,23 @@
+import pytest
+
+from dataclasses import FrozenInstanceError
+from svgtools.model.scene.transform.affine import Affine
+
+def test_affine_construction():
+    a = Affine(a=1, b=2, c=3, d=4, e=5, f=6)
+    assert a.a == 1
+    assert a.b == 2
+    assert a.c == 3
+    assert a.d == 4
+    assert a.e == 5
+    assert a.f == 6
+
+def test_affines_are_equal():
+    assert ( Affine(a=1, b=2, c=3, d=4, e=5, f=6) ==
+             Affine(a=1, b=2, c=3, d=4, e=5, f=6)
+           )
+
+def test_affine_is_immutable():
+    a = Affine(a=1, b=2, c=3, d=4, e=5, f=6)
+    with pytest.raises(FrozenInstanceError):
+        a.d = 5
---
+++ tests/model/scene/transform/test_skew_xy.py
@@ -0,0 +1,25 @@
+import pytest
+
+from dataclasses import FrozenInstanceError
+from svgtools.model.scene.transform.skew_x import SkewX
+from svgtools.model.scene.transform.skew_y import SkewY
+
+def test_skew_x_construction():
+    s = SkewX(theta=60)
+    assert s.theta == 60
+
+def test_skew_y_construction():
+    s = SkewY(theta=60)
+    assert s.theta == 60
+
+def test_skews_are_equal():
+    assert SkewX(theta=21) == SkewX(theta=21)
+    assert SkewY(theta=22) == SkewY(theta=22)
+
+def test_skews_are_immutable():
+    s = SkewX(theta=60)
+    with pytest.raises(FrozenInstanceError):
+        s.theta=61
+    s = SkewY(theta=60)
+    with pytest.raises(FrozenInstanceError):
+        s.theta=61
