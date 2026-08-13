c79d657131d815e69263eed9f5243b91692b3172
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-13 21:36:09 +0200

    refactor: change decomp implementation

--- tests/model/geometry/test_matrix3_decomp.py
+++ tests/model/geometry/test_matrix3_decomp.py
@@ -1,54 +1,68 @@
-from svgtools.model.geometry.matrix3 import Matrix3
+from svgtools.model.geometry.matrix3 import Matrix3, TRHxSDecomposition
 
 def test_matrix3_decomp_translate():
     mt = Matrix3.translation(2,3)
-    (m_T, m_R, m_Hx, m_S) = Matrix3.decompose(mt)
-    assert (m_T, m_R, m_Hx, m_S) == (
-        mt,
-        Matrix3.identity(),
-        Matrix3.identity(),
-        Matrix3.identity(),
+    md = Matrix3.TRHxS_decompose(mt)
+    assert md == TRHxSDecomposition(
+        tx= 2,
+        ty= 3,
+        theta_rotate= 0,
+        theta_skew_x= 0,
+        sx= 1,
+        sy= 1,
     )
 
 def test_matrix3_decomp_rotate():
     mr = Matrix3.rotation(60, 0, 0)
-    (m_T, m_R, m_Hx, m_S) = Matrix3.decompose(mr)
-    assert (m_T, m_R, m_Hx, m_S) == (
-        Matrix3.identity(),
-        mr,
-        Matrix3.identity(),
-        Matrix3.identity(),
+    md = Matrix3.TRHxS_decompose(mr)
+    assert md.isclose(TRHxSDecomposition(
+        tx= 0,
+        ty= 0,
+        theta_rotate= 60,
+        theta_skew_x= 0,
+        sx= 1,
+        sy= 1,
+        )
     )
 
 def test_matrix3_decomp_translate_and_rotate():
     mt = Matrix3.translation(2,3)
     mr = Matrix3.rotation(60, 0, 0)
-    (m_T, m_R, m_Hx, m_S) = Matrix3.decompose(mt*mr)
-    assert (m_T, m_R, m_Hx, m_S) == (
-        mt,
-        mr,
-        Matrix3.identity(),
-        Matrix3.identity(),
+    md = Matrix3.TRHxS_decompose(mt*mr)
+    assert md.isclose(TRHxSDecomposition(
+        tx= 2,
+        ty= 3,
+        theta_rotate= 60,
+        theta_skew_x= 0,
+        sx= 1,
+        sy= 1,
+        )
     )
 
 def test_matrix3_decomp_scale():
     ms = Matrix3.scaling(3,4)
-    (m_T, m_R, m_Hx, m_S) = Matrix3.decompose(ms)
-    assert (m_T, m_R, m_Hx, m_S) == (
-        Matrix3.identity(),
-        Matrix3.identity(),
-        Matrix3.identity(),
-        ms
+    md = Matrix3.TRHxS_decompose(ms)
+    assert md.isclose(TRHxSDecomposition(
+        tx= 0,
+        ty= 0,
+        theta_rotate= 0,
+        theta_skew_x= 0,
+        sx= 3,
+        sy= 4,
+        )
     )
 
 def test_matrix3_decomp_skewX():
     mh = Matrix3.skew_x(45)
-    (m_T, m_R, m_Hx, m_S) = Matrix3.decompose(mh)
-    assert (m_T, m_R, m_Hx, m_S) == (
-        Matrix3.identity(),
-        Matrix3.identity(),
-        mh,
-        Matrix3.identity(),
+    md = Matrix3.TRHxS_decompose(mh)
+    assert md.isclose(TRHxSDecomposition(
+        tx= 0,
+        ty= 0,
+        theta_rotate= 0,
+        theta_skew_x= 45,
+        sx= 1,
+        sy= 1,
+        )
     )
 
 def test_matrix3_decomp_any():
@@ -57,5 +71,10 @@ def test_matrix3_decomp_any():
         4,5,6,
         0,0,1
         )
-    (m_T, m_R, m_Hx, m_S) = Matrix3.decompose(m)
-    assert m.isclose(m_T * m_R * m_Hx * m_S)
+    md = Matrix3.TRHxS_decompose(m)
+    assert m.isclose(
+        Matrix3.translation(dx=md.tx, dy=md.ty) *
+        Matrix3.rotation(theta_degree=md.theta_rotate, cx=0, cy=0) *
+        Matrix3.skew_x(theta_degree=md.theta_skew_x) *
+        Matrix3.scaling(sx=md.sx, sy=md.sy)
+    )
