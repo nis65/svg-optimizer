62ca61c4ed11a1fe5cbd092220860d1a1c9b2245
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-13 21:39:54 +0200

    feat: allow strategy to pass to rewrite example

--- examples/rewrite.py
+++ examples/rewrite.py
@@ -6,11 +6,18 @@ import sys
 from svgtools.parser.svg_parser import parse_svg_string
 from svgtools.model.scene.document import Document
 from svgtools.writer.svg_writer import SvgWriter
+from svgtools.writer.transform_write_strategy import TransformWriteStrategy
+
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--in", dest="input")
 parser.add_argument("--out", dest="output")
+parser.add_argument("--strategy",
+                    type=TransformWriteStrategy.__getitem__,
+                    choices=TransformWriteStrategy,
+                    default=TransformWriteStrategy.KEEP
+                   )
 
 args = parser.parse_args()
 
@@ -20,7 +27,7 @@ outfile = open(args.output, "w", encoding="utf-8") if args.output else sys.stdou
 try:
     svg_input_text = infile.read()
     svg_doc = parse_svg_string(svg_input_text)
-    writer = SvgWriter()
+    writer = SvgWriter(args.strategy)
     outfile.write(writer.write_svg_string(svg_doc))
 
 finally:
@@ -28,5 +35,3 @@ finally:
         infile.close()
     if outfile is not sys.stdout:
         outfile.close()
-
-
