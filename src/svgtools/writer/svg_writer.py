651f5cdfe380231426961b22a435ca959758e50e
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-15 18:26:36 +0200

    feat: all transformation conversion modes

--- src/svgtools/writer/svg_writer.py
+++ src/svgtools/writer/svg_writer.py
@@ -10,6 +10,8 @@ from svgtools.model.scene.get_matrix import get_matrix, transforms_to_matrix
 from svgtools.model.geometry.rect import Rect as GeometryRect
 from svgtools.model.geometry.circle import Circle as GeometryCircle
 from svgtools.model.geometry.point import Point as GeometryPoint
+from svgtools.model.geometry.matrix3 import Matrix3, TRHxSDecomposition
+from svgtools.model.geometry.isclose import geometry_isclose
 
 class SvgWriter:
 
@@ -17,13 +19,21 @@ class SvgWriter:
     INDENT = "  "
     PRECISION = 3
 
-
     def __init__(
             self,
             strategy: TransformWriteStrategy = TransformWriteStrategy.KEEP
             ):
         self._parts: list[str] = []
         self.strategy = strategy
+        self.total_aggregated_chains = 0
+        self.total_aggressive_chains = 0
+
+    @property
+    def conservative_stats(self) -> tuple[int, int]:
+        return (
+            self.total_aggregated_chains,
+            self.total_aggressive_chains,
+        )
 
     def write_svg_string(self, document: Document) -> str:
         self._write_document(document)
@@ -131,19 +141,108 @@ class SvgWriter:
         match self.strategy:
             case TransformWriteStrategy.KEEP:
                 return transformations
-            # case TransformWriteStrategy.AGGREGATE :
-            # case TransformWriteStrategy.DECOMPOSE_MATRIX :
-            # case TransformWriteStrategy.AGGREGATE_AND_DECOMPOSE_MATRIX :
-            # case TransformWriteStrategy.CANONICAL_CONSERVATIVE :
+            case TransformWriteStrategy.AGGREGATE:
+                return self._strategy_aggregate(transformations)
+            case TransformWriteStrategy.DECOMPOSE_MATRIX:
+                return self._strategy_decompose_matrix(transformations)
+            case TransformWriteStrategy.DECOMPOSE_MATRIX_AND_AGGREGATE:
+                decomposed=self._strategy_decompose_matrix(transformations)
+                return self._strategy_aggregate(decomposed)
+            case TransformWriteStrategy.CANONICAL_CONSERVATIVE :
+                return self._strategy_conservative(transformations)
             case TransformWriteStrategy.CANONICAL_AGGRESSIVE:
                 return self._strategy_aggressive(transformations)
             case _:
                 raise ValueError(f"strategy {self.strategy} not implemented")
 
     @staticmethod
+    def _is_canonical(transformations) -> bool:
+        _CANONICAL_ORDER = (Translate, Rotate, SkewX, Scale)
+        reference = iter(_CANONICAL_ORDER)
+        for t in transformations:
+            if type(t) == Rotate:
+                if not (t.cx == 0 and t.cy == 0):
+                    return False
+            for expected in reference:
+                if type(t) == expected:
+                    break
+            else:
+                return False
+        return True
+
+    @staticmethod
+    def _strategy_aggregate(transformations):
+        if len(transformations) == 0:
+            raise ValueError(f"should not be called with 0 transformations")
+        t_list = []
+        agg_t = None
+        for t in transformations:
+            if agg_t is None:
+                agg_t = t
+            elif type(t) == type(agg_t):
+                match t:
+                    case Translate():
+                        agg_t = Translate(dx = agg_t.dx + t.dx, dy = agg_t.dy + t.dy)
+                    case Scale():
+                        agg_t = Scale(sx = agg_t.sx * t.sx, sy = agg_t.sy * t.sy)
+                    case Rotate():
+                        if agg_t.cx == t.cx and agg_t.cy == t.cy:
+                            agg_t = Rotate(theta = agg_t.theta + t.theta, cx = t.cx, cy = t.cy)
+                        else:
+                            # rotate can only be aggregated exactly with the same rotation center
+                            t_list.append(agg_t)
+                            agg_t = t
+                    case SkewX() | SkewY():
+                        # skew cannot be aggregated in an exact way
+                        t_list.append(agg_t)
+                        agg_t = t
+                    case Affine():
+                        agg_m = get_matrix(agg_t) * get_matrix(t)
+                        agg_t= Affine(a=agg_m.m11, b=agg_m.m21, c=agg_m.m12,
+                                      d=agg_m.m22, e=agg_m.m13, f=agg_m.m23)
+            else:
+                t_list.append(agg_t)
+                agg_t = t
+        t_list.append(agg_t)
+        return tuple(t_list)
+
+    @staticmethod
+    def _strategy_decompose_matrix(transformations):
+        t_list = []
+        for t in transformations:
+            match t:
+                case Affine():
+                    md = Matrix3.TRHxS_decompose(
+                            Matrix3.affine(a=t.a, b=t.b, c=t.c,
+                                           d=t.d, e=t.e, f=t.f
+                        )
+                    )
+                    if not (md.tx == 0 and md.ty == 0):
+                        t_list.append(Translate(dx=md.tx, dy=md.ty))
+                    if not geometry_isclose(md.theta_rotate, 0):
+                        t_list.append(Rotate(theta=md.theta_rotate, cx=0, cy=0))
+                    if not geometry_isclose(md.theta_skew_x, 0):
+                        t_list.append(SkewX(theta=md.theta_skew_x))
+                    if not (md.sx == 1 and md.sy == 1):
+                        t_list.append(Scale(sx=md.sx, sy=md.sy))
+                case _:
+                    t_list.append(t)
+        return tuple(t_list)
+
+    def _strategy_conservative(self, transformations):
+        # attempt aggregate
+        aggregated = self._strategy_aggregate(transformations)
+        if self._is_canonical(aggregated):
+            self.total_aggregated_chains +=1
+            return aggregated
+        else:
+            self.total_aggressive_chains +=1
+            return self._strategy_aggressive(transformations)
+
+    @staticmethod
     def _strategy_aggressive(transformations):
         m = transforms_to_matrix(transformations)
-        return (Affine(a=m.m11, b=m.m21, c=m.m12, d=m.m22, e=m.m13, f=m.m23),)
+        return SvgWriter._strategy_decompose_matrix((Affine(a=m.m11, b=m.m21, c=m.m12, d=m.m22, e=m.m13, f=m.m23),))
 
     @staticmethod
     def _number_to_string(number: float | str) -> str:
