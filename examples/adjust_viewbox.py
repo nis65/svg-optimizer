f538a100f7b7d3b308d45e668e92c9e204edc6a5
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-16 00:21:18 +0200

    feat: example program: adjust_viewbox

---
+++ examples/adjust_viewbox.py
@@ -0,0 +1,64 @@
+import argparse
+import sys
+from dataclasses import replace
+
+from svgtools.parser.svg_parser import parse_svg_string
+from svgtools.semantic.bounding_box_visitor import BoundingBoxVisitor
+from svgtools.writer.svg_writer import SvgWriter
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--in", dest="input")
+parser.add_argument("--out", dest="output")
+
+def margin(value):
+    value = int(value)
+    if not 0 <= value <= 40:
+        raise argparse.ArgumentTypeError(f"margin must be between 0 and 40 percent, not {value}")
+    return value
+
+parser.add_argument("--margin", type=margin, default=5)
+
+args = parser.parse_args()
+infile = open(args.input, encoding="utf-8") if args.input else sys.stdin
+outfile = open(args.output, "w", encoding="utf-8") if args.output else sys.stdout
+
+try:
+    # get original document and viewBox width and height
+    svg_input_text = infile.read()
+    svg_doc = parse_svg_string(svg_input_text)
+    viewbox_width = svg_doc.svg.viewBox[2]
+    viewbox_height = svg_doc.svg.viewBox[3]
+
+    # get boundingbox width and height
+    bbvisitor = BoundingBoxVisitor()
+    bbvisitor.visit(svg_doc)
+    bbwidth  = bbvisitor.bounding_box.max.x - bbvisitor.bounding_box.min.x
+    bbheight = bbvisitor.bounding_box.max.y - bbvisitor.bounding_box.min.y
+
+    # compute new viewbox width and height, keeping aspect ratio of old viewBox
+    framefactor = 1 + (( 2 * args.margin ) / 100 )
+    minimal_viewbox_width = bbwidth * framefactor
+    minimal_viewbox_height = bbheight * framefactor
+    if bbwidth / bbheight > viewbox_width / viewbox_height:
+        new_viewbox_width = minimal_viewbox_width
+        new_viewbox_height = new_viewbox_width * viewbox_height / viewbox_width
+    else:
+        new_viewbox_height = minimal_viewbox_height
+        new_viewbox_width  = new_viewbox_height * viewbox_width / viewbox_height
+
+    # compute new viewbox topleft corner
+    new_viewbox_topleft_x = bbvisitor.bounding_box.min.x - (new_viewbox_width - bbwidth) / 2
+    new_viewbox_topleft_y = bbvisitor.bounding_box.min.y - (new_viewbox_height - bbheight) / 2
+
+    # replace viewbox in document
+    new_svg = replace(svg_doc.svg, viewBox=(new_viewbox_topleft_x, new_viewbox_topleft_y, new_viewbox_width, new_viewbox_height))
+    new_svg_doc = replace(svg_doc, svg=new_svg)
+
+    # and write document
+    writer = SvgWriter()
+    outfile.write(writer.write_svg_string(new_svg_doc))
+        
+finally:
+    if infile is not sys.stdin:
+        infile.close()
+
