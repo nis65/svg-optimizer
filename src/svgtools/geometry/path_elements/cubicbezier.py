feb52bb1a2f99060d3c2c002b75ef95aacf39894
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-19 08:26:49 +0200

    feat: cubic bezier

--- src/svgtools/geometry/path_elements/cubicbezier.py
+++ src/svgtools/geometry/path_elements/cubicbezier.py
@@ -5,18 +5,19 @@ from .path_element_abc import PathElement
 from ..point import Point
 
 @dataclass(frozen=True, slots=True)
-class QuadraticBezier(PathElement):
+class CubicBezier(PathElement):
 
     control1: Point
+    control2: Point
     end: Point
     representation: str
 
     def __post_init__(self) -> None:
-        if not (self.representation == 'q' or self.representation == 'Q'
-             or self.representation == 't' or self.representation == 'T'
+        if not (self.representation == 'c' or self.representation == 'C'
+             or self.representation == 's' or self.representation == 'S'
             ):
             raise ValueError(
-                f"QuadraticBezier can only be represented by one of 'qQtT', not {self.representation}"
+                f"CubicBezier can only be represented by one of 'cCsS', not {self.representation}"
             )
 
     @property
@@ -24,12 +25,12 @@ class QuadraticBezier(PathElement):
         return self.end
 
     @staticmethod
-    def _qb(p0: float, p1: float, p2: float, t: float) -> float:
-        return (1-t)*(1-t)*p0 + 2*(1-t)*t*p1 + t*t*p2
+    def _qb(p0: float, p1: float, p2: float, p3: float, t: float) -> float:
+        return (1-t)*(1-t)*(1-t)*p0 + 3*(1-t)*(1-t)*t*p1 + 3*(1-t)*t*t*p2 + t*t*t*p3
 
     def _point_at(self, start: Point, t: float) -> Point:
-        new_x = self._qb(start.x, self.control1.x, self.end.x, t)
-        new_y = self._qb(start.y, self.control1.y, self.end.y, t)
+        new_x = self._qb(start.x, self.control1.x, self.control2.x, self.end.x, t)
+        new_y = self._qb(start.y, self.control1.y, self.control2.y, self.end.y, t)
         return Point(x = new_x, y=new_y)
 
     def points_for_bounding_box(self, start: Point, count: int) -> set[Point]:
