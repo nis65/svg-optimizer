e8ea7d308666fe650f86c6a9f1329642c19606df
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-19 05:19:37 +0200

    refactor: do not inherit from geometry_abc any more

--- src/svgtools/geometry/path_elements/path_element_abc.py
+++ src/svgtools/geometry/path_elements/path_element_abc.py
@@ -1,6 +1,14 @@
 from abc import ABC, abstractmethod
+from ..point import Point
 
-from ..geometry_abc import Geometry
-
-class PathElement(Geometry, ABC):
+class PathElement(ABC):
     representation: str
+
+    @property
+    @abstractmethod
+    def endpoint(self) -> Point:
+        ...
+
+    @abstractmethod
+    def points_for_bounding_box(self, start: Point, number_of_points: int) -> set[Point]:
+        ...
