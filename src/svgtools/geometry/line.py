d9a24d5bd4b941e25feaa1d364e163ed85546c59
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-21 17:57:28 +0200

    feat: geometry for line, polyline, polygon

---
+++ src/svgtools/geometry/line.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from .geometry_abc import Geometry
+from .point import Point
+from .bounding_box import BoundingBox
+
+@dataclass(frozen=True, slots=True)
+class Line(Geometry):
+    start: Point
+    end: Point
+
+    def points_for_bounding_box(self, count: int) -> set[Point]:
+        # count is ignored, as the two points fully define the bounding box
+        return {
+            self.start,
+            self.end
+        }
