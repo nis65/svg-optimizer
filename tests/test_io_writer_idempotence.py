ee2f6d248c8725597f4af0947816afc3c1242857
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-21 20:40:00 +0200

    test: make io writer run over all testfiles

--- tests/test_io_writer_idempotence.py
+++ tests/test_io_writer_idempotence.py
@@ -1,35 +1,12 @@
 from pathlib import Path
-DATA = Path(__file__).parent / "testdata"
-
+import pytest
 from svgtools.io import write_svg_file, parse_svg_file
 
-def test_io_document_roundtrip_logo(tmp_path):
+DATA = Path(__file__).parent / "testdata"
 
-    start = DATA / "logo.svg"
+@pytest.mark.parametrize("start", DATA.glob("*.svg"), ids=lambda p: p.name)
 
-    gen1 = tmp_path / "gen1.svg"
-    write_svg_file(parse_svg_file(start), gen1)
-
-    gen2 = tmp_path / "gen2.svg"
-    write_svg_file(parse_svg_file(gen1), gen2)
-
-    assert gen1.read_bytes() == gen2.read_bytes()
-
-def test_io_document_roundtrip_ellipse(tmp_path):
-
-    start = DATA / "transformed_circle.svg"
-
-    gen1 = tmp_path / "gen1.svg"
-    write_svg_file(parse_svg_file(start), gen1)
-
-    gen2 = tmp_path / "gen2.svg"
-    write_svg_file(parse_svg_file(gen1), gen2)
-
-    assert gen1.read_bytes() == gen2.read_bytes()
-
-def test_io_document_roundtrip_more_transforms(tmp_path):
-
-    start = DATA / "simple_transforms.svg"
+def test_io_writer_idempotence(start, tmp_path):
 
     gen1 = tmp_path / "gen1.svg"
     write_svg_file(parse_svg_file(start), gen1)
