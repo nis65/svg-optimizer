ba0aa47c3c916ab7b0a72d1143404816d1214290
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-21 17:57:28 +0200

    feat: geometry for line, polyline, polygon

---
+++ src/svgtools/geometry/polygon.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from .geometry_abc import Geometry
+from .point import Point
+from .bounding_box import BoundingBox
+
+@dataclass(frozen=True, slots=True)
+class Polygon(Geometry):
+    children: tuple = ()
+
+    def points_for_bounding_box(self, count: int) -> set[Point]:
+        # count is ignored, as the n points fully define the bounding box
+        points = []
+        for point in self.children:
+            points.append(point)
+        return set(points)
