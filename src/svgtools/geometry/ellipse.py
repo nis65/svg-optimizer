976e537eae56c7eeb86ec09f2f1b5de2655df558
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-21 20:08:55 +0200

    feat: ellipse geometry

---
+++ src/svgtools/geometry/ellipse.py
@@ -0,0 +1,27 @@
+import math
+
+from dataclasses import dataclass
+from .point import Point
+from .geometry_abc import Geometry
+from .bounding_box import BoundingBox
+
+@dataclass(frozen=True, slots=True)
+class Ellipse(Geometry):
+    center: Point
+    radiusx: float
+    radiusy: float
+
+    def __post_init__(self) -> None:
+        if self.radiusx < 0 or self.radiusy < 0:
+            raise ValueError(
+                f"both radius x ({self.radiusx}) and radius y ({self.radiusy}) must not be negative"
+            )
+
+    def points_for_bounding_box(self, count: int) -> set[Point]:
+        points = []
+        for i in range(count):
+            theta = (2 * math.pi * i ) / count
+            x = self.center.x + self.radiusx * math.cos(theta)
+            y = self.center.y + self.radiusy * math.sin(theta)
+            points.append(Point(x,y))
+        return set(points)
