0d67ee9adaf3d61d5367481d4cb598053ca77e03
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-22 17:31:19 +0200

    test: compact path

--- tests/writer/test_writer_path_modes.py
+++ tests/writer/test_writer_path_modes.py
@@ -84,4 +84,48 @@ def test_write_absolute_canonical_base():
     </svg>
     """)
 # }}}
+# {{{ document "e" for PathCompactness
+e = Document(
+    svg=Svg(
+        children=(
+            Shape (
+                id="mypath",
+                geometry = Path (
+                    children = (
+                        MoveTo(target = Point(x = 30, y = 50,), representation = 'm',),
+                        LineTo(target = Point(x = 70, y = 30,), representation = 'l',),
+                        LineTo(target = Point(x = 70, y = 40,), representation = 'l',),
+                        LineTo(target = Point(x = 80, y = 40,), representation = 'l',),
+                        ClosePath(representation = 'z',),
+                        LineTo(target = Point(x = 50, y = 80,), representation = 'l',),
+                        QuadraticBezier(control1 = Point(x = 70, y = 110,), end = Point(x = 90, y = 80,), representation = 'q',),
+                        QuadraticBezier(control1 = Point(x = 110, y = 50,), end = Point(x = 130, y = 80,), representation = 'q',),
+                        LineTo(target = Point(x = 50, y = 150,), representation = 'l',),
+                        CubicBezier(control1 = Point(x = 90, y = 170,), control2 = Point(x = 30, y = 170,), end = Point(x = 70, y = 150,), representation = 'c',),
+                        CubicBezier(control1 = Point(x = 110, y = 130,), control2 = Point(x = 50, y = 130,), end = Point(x = 90, y = 150,), representation = 'c',),
+                        LineTo(target = Point(x = 130, y = 170,), representation = 'l',),
+                        Arc(rx = 50, ry = 40, phi = 135, large_arc_flag = 0, sweep_flag = 0, end = Point(x = 170, y = 90,), representation = 'a'),
+                    ),
+                ),
+            ),
+        ),
+    ),
+)
+
+
+
+# }}}
+# {{{ def test_write_absolute_compact_base():
+def test_write_absolute_canonical_base():
+    writer = SvgWriter(path_coordinates = PathCoordinates.ABSOLUTE,
+                       path_compactness = PathCompactness.COMPACT,
+                       path_command_set = PathCommandSet.BASE
+                      )
+    assert writer.write_svg_string(e) == dedent("""\
+    <?xml version='1.0' encoding='UTF-8'?>
+    <svg>
+    <path id="mypath" d="M 30 50 70 30 70 40 80 40 Z L 50 80 Q 70 110 90 80 110 50 130 80 L 50 150 C 90 170 30 170 70 150 110 130 50 130 90 150 L 130 170 A 50 40 135 0 0 170 90" />
+    </svg>
+    """)
+# }}}
 
