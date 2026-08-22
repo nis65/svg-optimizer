94ae8bc1709c126a14d7402494423c6e0f30b2f5
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-22 17:32:03 +0200

    feat: improve example scripts for path modes

---
+++ examples/compare_path_options.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+TARGETDIR=/tmp/path_options
+
+if [[ -r $TARGETDIR && -w $TARGETDIR && -d $TARGETDIR ]]
+then
+  true
+else
+  mkdir $TARGETDIR
+fi
+rm -rf $TARGETDIR/*
+
+echo "Results in $TARGETDIR"
+
+(
+
+cd $(dirname $0)
+
+for file in ../tests/testdata/*svg
+do
+  basename=$( basename $file )
+  echo -n "${basename}: "
+  THISTARGETDIR=$TARGETDIR/${basename%%.svg}
+  mkdir $THISTARGETDIR
+  cp "$file" $THISTARGETDIR/ORIG.svg
+
+  for coord in ABSOLUTE RELATIVE KEEP
+  do
+    for compact in CANONICAL COMPACT
+    do
+      for commandset in BASE FULL
+      do
+        targetname=$THISTARGETDIR/${coord}_${compact}_${commandset}.svg
+        ./run_rewrite.sh --path-coordinates ${coord} --path-compactness ${compact} --path-command-set ${commandset} \
+		         < $file > $targetname
+	echo "$targetname"
+      done
+    done
+  done
+done
+)
+
+
+
--- examples/rewrite.py
+++ examples/rewrite.py
@@ -4,6 +4,7 @@ import sys
 from svgtools.parser.svg_parser import parse_svg_string
 from svgtools.writer.svg_writer import SvgWriter
 from svgtools.writer.transform_write_strategy import TransformWriteStrategy
+from svgtools.writer.path_write_options import PathCoordinates, PathCompactness, PathCommandSet
 
 parser = argparse.ArgumentParser()
 
@@ -13,7 +14,33 @@ parser.add_argument("--transform-strategy",
                     dest="transform_strategy",
                     type=TransformWriteStrategy.__getitem__,
                     choices=TransformWriteStrategy,
-                    default=TransformWriteStrategy.KEEP
+                    metavar="{" + ",".join(e.name for e in TransformWriteStrategy) + "}",
+                    default=TransformWriteStrategy.KEEP,
+                    help="transformation conversion strategy (default: KEEP)",
+                   )
+parser.add_argument("--path-coordinates",
+                    dest="path_coordinates",
+                    type=PathCoordinates.__getitem__,
+                    choices=PathCoordinates,
+                    metavar="{" + ",".join(e.name for e in PathCoordinates) + "}",
+                    default=PathCoordinates.ABSOLUTE,
+                    help="coordinate representation in path (default: ABSOLUTE)",
+                   )
+parser.add_argument("--path-compactness",
+                    dest="path_compactness",
+                    type=PathCompactness.__getitem__,
+                    choices=PathCompactness,
+                    metavar="{" + ",".join(e.name for e in PathCompactness) + "}",
+                    default=PathCompactness.CANONICAL,
+                    help="Compact adjacent identical commands in path (default: CANONICAL)",
+                   )
+parser.add_argument("--path-command-set",
+                    dest="path_command_set",
+                    type=PathCommandSet.__getitem__,
+                    choices=PathCommandSet,
+                    metavar="{" + ",".join(e.name for e in PathCommandSet) + "}",
+                    default=PathCommandSet.BASE,
+                    help="Command set used in path (default: BASE)"
                    )
 
 args = parser.parse_args()
@@ -24,7 +51,11 @@ outfile = open(args.output, "w", encoding="utf-8") if args.output else sys.stdou
 try:
     svg_input_text = infile.read()
     svg_doc = parse_svg_string(svg_input_text)
-    writer = SvgWriter(transform_strategy = args.transform_strategy)
+    writer = SvgWriter(transform_strategy = args.transform_strategy,
+                       path_coordinates = args.path_coordinates,
+                       path_compactness = args.path_compactness,
+                       path_command_set = args.path_command_set,
+                      )
     outfile.write(writer.write_svg_string(svg_doc))
     if args.transform_strategy == TransformWriteStrategy.CANONICAL_CONSERVATIVE:
        print(f"conservative stats (exact, forced): {writer.conservative_stats}", file=sys.stderr)
