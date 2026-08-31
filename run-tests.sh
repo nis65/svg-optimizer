27a42444ac806e7f4fb9d640c5598544681252a8
Nicolas Iselin <nicolas.github@iselin.ch>
2026-08-31 23:04:38 +0200

    feat: integrate pyright into run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -4,6 +4,7 @@ set -e
 ruff format --check
 ruff check --preview --extend-select PLR --output-format=full src
 pytest --cov=. --cov-report=term-missing --cov-fail-under=100
+pyright src
 
 for i in all_boundingboxes.sh all_path_options.sh all_strats.sh
 do
